0c6f750316a0a3f6b21d41873912962b40d4ec25
m-atkins <matthewatkins16@gmail.com>
2023-11-25 16:02:48 +0000

    second

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,65 @@
 import * as THREE from 'three';
-
-const div = document.getElementById('myDiv');
-
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+let mixer;
+
+const loader = new GLTFLoader();
+loader.load( 'src/media/models/wizard_cat.glb', function ( gltf ) {
+
+	let mesh = gltf
+
+// Create an AnimationMixer, and get the list of AnimationClip instances
+const model = gltf.scene
+mixer = new THREE.AnimationMixer( model );
+const clips = mesh.animations;
+
+// function update () {
+// 	mixer.update( deltaSeconds );
+// }
+
+console.log(clips)
+
+const clip = THREE.AnimationClip.findByName( clips, 'Animation' );
+const action = mixer.clipAction( clip );
+action.play();
+
+scene.add( gltf.scene );
+console.log("potato!")
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-//document.body.appendChild( renderer.domElement );
-div.appendChild(renderer.domElement);
+document.body.appendChild( renderer.domElement );
 
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// scene.add( cube );
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
 
 camera.position.z = 5;
+// const controls = new OrbitControls( camera, renderer.domElement );
 
+const clock = new THREE.Clock();
 function animate() {
 	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
+	mixer.update(clock.getDelta());
+	// cube.rotation.x += 0.01;
+	// cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
 
 animate();
+
+// renderer.setAnimationLoop(animate);
