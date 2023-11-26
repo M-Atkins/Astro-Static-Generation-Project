04c2bd66b7956628aed0bd6c4087bab086ab374f
m-atkins <matthewatkins16@gmail.com>
2023-11-26 19:10:12 +0000

    commit

---
+++ src/posts/bugatti.md
@@ -0,0 +1,11 @@
+---
+title: Bought a bugatti!
+slug: bugatti
+excerpt: Did I buy a bugatti? read more to find out!
+date: 2024-05-13
+author: Bugatti Steve
+tags: ['cars, money, women']
+---
+
+I love bugatti cars! 
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 let mixer;
@@ -36,10 +37,13 @@ console.log("potato!")
 } );
 
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
+// const renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer( { alpha: true, antialias: true } );
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize( 320, 400 );
+// document.body.appendChild( renderer.domElement );
+const div = document.getElementById('threediv');
+div.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -49,12 +53,19 @@ const light = new THREE.AmbientLight( 0x404040 ); // soft white light
 scene.add( light );
 
 camera.position.z = 5;
-// const controls = new OrbitControls( camera, renderer.domElement );
+const orbit = new OrbitControls( camera, renderer.domElement );
+
+orbit.autoRotate = true;
+orbit.enablePan = false;
+orbit.enableZoom = false;
+orbit.enabled = false;
 
 const clock = new THREE.Clock();
 function animate() {
 	requestAnimationFrame( animate );
 	mixer.update(clock.getDelta());
+	orbit.update();
+	
 	// cube.rotation.x += 0.01;
 	// cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
