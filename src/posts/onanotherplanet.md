3d1bc5bb22fdeaeb37e1be3415560e42a42c00a2
m-atkins <matthewatkins16@gmail.com>
2024-01-09 12:07:54 +0000

    update

--- src/posts/onanotherplanet.md
+++ src/posts/onanotherplanet.md
@@ -1,10 +1,10 @@
 ---
 title: I've accidentally run to mars
 slug: run-to-mars
-excerpt: mars dude!
+excerpt: mars!
 date: 2024-05-13
 author: Mr Darcy
-tags: ['love', 'war', 'aa', 'apples']
+tags: ['space', 'rocks']
 ---
 
 I was on a jog and I ran to mars on accident!
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,40 +1,71 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
+
 const scene = new THREE.Scene();
+
+// instantiate a loader
+const loader = new OBJLoader();
+
+// load a resource
+loader.load(
+	// resource URL
+	'src/media/models/cubeme.obj',
+	// called when resource is loaded
+	function ( object ) {
+
+		scene.add( object );
+
+	},
+	// called when loading is in progresses
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+
 // const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-let mixer;
-
-const loader = new GLTFLoader();
-loader.load( 'src/media/models/wizard_cat.glb', function ( gltf ) {
-
-	let mesh = gltf
-
-// Create an AnimationMixer, and get the list of AnimationClip instances
-const model = gltf.scene
-mixer = new THREE.AnimationMixer( model );
-const clips = mesh.animations;
-
-// function update () {
-// 	mixer.update( deltaSeconds );
-// }
-
-console.log(clips)
-
-const clip = THREE.AnimationClip.findByName( clips, 'Animation' );
-const action = mixer.clipAction( clip );
-action.play();
-
-scene.add( gltf.scene );
-console.log("potato!")
-
-}, undefined, function ( error ) {
-
-	console.error( error );
-
-} );
+// let mixer;
+//
+// const loader = new GLTFLoader();
+// loader.load( 'src/media/models/wizard_cat.glb', function ( gltf ) {
+//
+// 	let mesh = gltf
+//
+// // Create an AnimationMixer, and get the list of AnimationClip instances
+// const model = gltf.scene
+// mixer = new THREE.AnimationMixer( model );
+// const clips = mesh.animations;
+//
+// // function update () {
+// // 	mixer.update( deltaSeconds );
+// // }
+//
+// console.log(clips)
+//
+// const clip = THREE.AnimationClip.findByName( clips, 'Animation' );
+// const action = mixer.clipAction( clip );
+// action.play();
+//
+// scene.add( gltf.scene );
+// console.log("potato!")
+//
+// }, undefined, function ( error ) {
+//
+// 	console.error( error );
+//
+// } );
 
 
 // const renderer = new THREE.WebGLRenderer();
@@ -55,10 +86,10 @@ scene.add( light );
 camera.position.z = 5;
 const orbit = new OrbitControls( camera, renderer.domElement );
 
-orbit.autoRotate = true;
-orbit.enablePan = false;
-orbit.enableZoom = false;
-orbit.enabled = false;
+// orbit.autoRotate = true;
+// orbit.enablePan = false;
+// orbit.enableZoom = false;
+// orbit.enabled = false;
 
 const clock = new THREE.Clock();
 function animate() {
